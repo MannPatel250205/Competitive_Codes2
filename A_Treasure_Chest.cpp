64297d2b9e8890eeb0433e5648c43348031397e2
MannPatel250205 <142524409+MannPatel250205@users.noreply.github.com>
2023-11-04 15:41:27 +0530

    A_Treasure_Chest.cpp

---
+++ A_Treasure_Chest.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+#define vei vector<int>
+#define vel vector<long long>
+#define mai map<int, int>
+#define mal map<long long, long long>
+#define srt(a) sort(a.begin(), a.end())
+#define srtd(a) sort(a.begin(), a.end(), greater<int>())
+#define fast ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+/***************                NEVER MIND THIS CODE                ***************/
+using namespace std;
+
+bool isprime(int n)
+{
+    if (n < 2)
+    {
+        return false;
+    }
+    for (int i = 2; i <= sqrt(n); ++i)
+    {
+        if (n % i == 0)
+        {
+            return false;
+        }
+    }
+ 
+    return true;
+}
+
+/****************            Ha bas, aa rahyo code            ****************/
+void aa_rahyo_code(){
+    int x, y, k;
+
+    cin >> x >> y >> k;
+
+    if (y <= x)
+    {
+        cout << x << '\n';
+        return;
+    }
+    
+    if (y - x <= k)
+    {
+        cout << y << '\n';
+        return;
+    }
+    
+    cout << x + k + (2 * (y - x - k)) << '\n';;
+}
+
+int main(){
+    fast;
+    int z;
+    cin >> z;
+    while (z--)
+    {
+        aa_rahyo_code();
+    }
+    return 0;
+}
